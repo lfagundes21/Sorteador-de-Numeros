1deecde6f54f35b121f7fb3ba6ae7d2c8c9cac0d
lfagundes21 <lfdsilva@gmail.com>
2024-07-22 18:21:57 -0300

    Criação de bloqueios para evitar erros de usuário

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ function sortear (){
     let sorteados = [];
     let numero;
 
+    if (limiteInferior > limiteSuperior){ //valida se o limite inferior é maior do que o Superior
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!'); // se positivo, gera alerta de erro
+        return; // para a execução do código
+    }
+
+    if (quantidadeDeNumeros >( limiteSuperior- limiteInferior +1)){ //valida se o limite inferior é maior do que o Superior
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!'); // se positivo, gera alerta de erro
+        return; // para a execução do código
+    }
+
     for (let i = 0; i < quantidadeDeNumeros; i++){ // loop limitado a quantidade de numeros solicitados
         numero = gerarNumeroAleatorio(limiteInferior, limiteSuperior); // chama a função gerarNumeroAleatorio levanto em conta os limites como parametros
         while(sorteados.includes(numero)){
